35b8d91bdd58374f804d6b98f4d22e243a98a0f9
Fred Zhao <829693+frdzy@users.noreply.github.com>
2024-10-28 20:56:40 -0700

    Reverse scan algorithm complete and tested

--- background.js
+++ background.js
@@ -37,21 +37,144 @@ function detachDebugger(tabId) {
 }
 
 function onEvent(debuggeeId, message, params) {
-  if (message === "Network.responseReceived") {
-    chrome.debugger.sendCommand(
-      {tabId: debuggeeId.tabId},
-      "Network.getResponseBody",
-      {requestId: params.requestId},
-      (response) => {
-        if (chrome.runtime.lastError) {
-          console.error(chrome.runtime.lastError);
-          return;
-        }
-        console.log("URL:", params.response.url);
-        console.log("Response Body:", response.body);
-      }
-    );
+  if (
+    message !== "Network.responseReceived" ||
+    // Check if the URL matches exactly -- 12465 appears to be the Goldman Tennis Center location ID
+    params.response.url !== "https://app.courtreserve.com/Online/Reservations/ReadConsolidated/12465"
+  ) {
+    return;
   }
+  chrome.debugger.sendCommand(
+    {tabId: debuggeeId.tabId},
+    "Network.getResponseBody",
+    {requestId: params.requestId},
+    (response) => {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+        return;
+      }
+      console.log("URL:", params.response.url);
+      // console.log("Response Body:", response.body);
+      const results = parseResponseBody(response.body);
+      console.log("Results:", results);
+    }
+  );
+}
+
+/**
+ * Given an example body with a list of objects, each representing a court reservation,
+ * augment the object with a new key "availabilityDuration" with values 30, 60, or 90.
+ * The duration is calculated by examining adjacent objects and seeing if any AvailableCourtIds
+ * overlap. If they do, the duration is the difference between the start of the first and end of the last.
+ * Note that three or more adjacent objects can have overlapping AvailableCourtIds, in which case the
+ * duration is the difference between the start of the first and end of the last, capped at 90.
+ * 
+ * {
+ *   "Data": [
+ *     {
+ *       "Id": "Hard10/27/2024 14:00:00",
+ *       "Title": null,
+ *       "Description": null,
+ *       "IsAllDay": false,
+ *       "Start": "/Date(1730037600000)/",
+ *       "End": "/Date(1730039400000)/",
+ *       "StartTimezone": null,
+ *       "EndTimezone": null,
+ *       "RecurrenceRule": null,
+ *       "RecurrenceException": null,
+ *       "CourtType": "Hard",
+ *       "AvailableCourts": 1,
+ *       "IsClosed": false,
+ *       "IsInPast": true,
+ *       "IsWaitListSlot": false,
+ *       "ShowWaitList": false,
+ *       "WaitListCount": null,
+ *       "IsAvailableTemplate": false,
+ *       "IsCourtAssignmentHiddenOnPortal": false,
+ *       "MemberIds": [],
+ *       "AvailableCourtIds": [51369],
+ *       "QueuedMembers": [],
+ *       "ShowCourtWaitlistOrderNumber": false
+ *     },
+ *     {
+ *       "Id": "Hard10/27/2024 14:30:00",
+ *       "Title": null,
+ *       "Description": null,
+ *       "IsAllDay": false,
+ *       "Start": "/Date(1730039400000)/",
+ *       "End": "/Date(1730041200000)/",
+ *       "StartTimezone": null,
+ *       "EndTimezone": null,
+ *       "RecurrenceRule": null,
+ *       "RecurrenceException": null,
+ *       "CourtType": "Hard",
+ *       "AvailableCourts": 1,
+ *       "IsClosed": false,
+ *       "IsInPast": true,
+ *       "IsWaitListSlot": false,
+ *       "ShowWaitList": false,
+ *       "WaitListCount": null,
+ *       "IsAvailableTemplate": false,
+ *       "IsCourtAssignmentHiddenOnPortal": false,
+ *       "MemberIds": [],
+ *       "AvailableCourtIds": [51369],
+ *       "QueuedMembers": [],
+ *       "ShowCourtWaitlistOrderNumber": false
+ *     }
+ *   ]
+ * }
+ * 
+ * @param {*} body 
+ */
+function parseResponseBody(body) {
+    const data = JSON.parse(body);
+    if (!data || !Array.isArray(data.Data)) {
+        return null;
+    }
+
+    const results = [];
+    const availabilitiesByCourtIds = {};
+    for (const slot of data.Data.reverse()) {
+        let maxDuration = 0;
+        const slotStartResult = slot.Start.match(/[0-9]+/);
+        if (!slotStartResult) {
+            continue;
+        }
+        const slotStartCleaned = +slotStartResult[0];
+        for (const courtId of slot.AvailableCourtIds) {
+            console.log('availabilitiesByCourtIds', availabilitiesByCourtIds);
+            if (!availabilitiesByCourtIds[courtId]) {
+                availabilitiesByCourtIds[courtId] = {};
+            }
+            const duration = getAvailableDurationFromStartTime(
+                availabilitiesByCourtIds[courtId], 
+                slotStartCleaned,
+            );
+            availabilitiesByCourtIds[courtId][slotStartCleaned] = duration;
+            maxDuration = Math.max(maxDuration, duration);
+        }
+        results.push({
+            StartTime: new Date(slotStartCleaned),
+            AvailabilityDuration: maxDuration,
+        });
+    }
+
+    return results;
+}
+
+function getAvailableDurationFromStartTime(availabilitiesForCourtId, startTime) {
+  let duration = 30;
+  // If availabilitiesForCourtId[startTime - 30 minutes] is available, set duration to 60;
+  // if availabilitiesForCourtId[startTime - 60 minutes] is ALSO available, set duration to 90
+  if (availabilitiesForCourtId[startTime + 30 * 60 * 1000]) {
+    console.log(60);
+    duration = 60;
+    if (availabilitiesForCourtId[startTime + 90 * 60 * 1000]) {
+      console.log(90);
+      duration = 90;
+    }
+  }
+  return duration;
 }
 
 chrome.runtime.onInstalled.addListener(() => {
