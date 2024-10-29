6fd72eda6376b357050fca23986eedfb0b0b8844
Fred Zhao <829693+frdzy@users.noreply.github.com>
2024-10-28 20:56:40 -0700

    Initial commit

---
+++ background.js
@@ -0,0 +1,56 @@
+let attachedTabId = null;
+
+chrome.action.onClicked.addListener((tab) => {
+  if (attachedTabId === tab.id) {
+    detachDebugger(tab.id);
+  } else {
+    attachDebugger(tab.id);
+  }
+});
+
+function attachDebugger(tabId) {
+  chrome.debugger.attach({tabId: tabId}, "1.0", () => {
+    if (chrome.runtime.lastError) {
+      console.error(chrome.runtime.lastError);
+      return;
+    }
+    console.log("Debugger attached");
+    attachedTabId = tabId;
+    chrome.debugger.sendCommand({tabId: tabId}, "Network.enable");
+    chrome.debugger.onEvent.addListener(onEvent);
+  });
+}
+
+function detachDebugger(tabId) {
+  chrome.debugger.detach({tabId: tabId}, () => {
+    if (chrome.runtime.lastError) {
+      console.error(chrome.runtime.lastError);
+      return;
+    }
+    console.log("Debugger detached");
+    attachedTabId = null;
+    chrome.debugger.onEvent.removeListener(onEvent);
+  });
+}
+
+function onEvent(debuggeeId, message, params) {
+  if (message === "Network.responseReceived") {
+    chrome.debugger.sendCommand(
+      {tabId: debuggeeId.tabId},
+      "Network.getResponseBody",
+      {requestId: params.requestId},
+      (response) => {
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError);
+          return;
+        }
+        console.log("URL:", params.response.url);
+        console.log("Response Body:", response.body);
+      }
+    );
+  }
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('Chrome extension installed!');
+});
